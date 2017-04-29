3ded23f8a74012712a217899525cad84d8199330
martinsflavio <flaviodosreismartins@gmail.com>
2017-04-29 15:01:11 -0700

    adding db connection

--- app/data/network_db.js
+++ app/data/network_db.js
@@ -4,10 +4,11 @@ const mysql = require('mysql');
 // connect with data base
 
 const connection = mysql.createConnection({
-  host        : 'localhost',
-  user        : 'root',
-  password    : '',
-  database    : 'network_db'
+  host        : 'ec2-50-19-95-47.compute-1.amazonaws.com',
+  user        : 'jhmzwqdqylvaom',
+  password    : '42139e9b0130bda9498548fe4adca06ec0b4a5cbd2624f14031b77fcb7fb6860',
+  port        : '5432',
+  database    : 'deu64qpvqafpo1'
 });
 
 
